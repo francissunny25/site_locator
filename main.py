a3ca487f9bc81f029a7910031a836f0aff14c02c
francissunny25 <francis.sunny.25@gmail.com>
2024-05-01 22:31:39 +0100

    created project structure

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.logger import logging
+
+if __name__ == "__main__":
+    logging.info("Collecting data from the user")
+    post_code = 'RG1 3PR'
+    
+    # job_reader = JobReader(job_title)
